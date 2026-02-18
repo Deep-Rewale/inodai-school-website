5a642b0a9ec37794a4c691cb62d196d1ed6c4589
Deep-Rewale <deeprewale2004@gmail.com>
2026-02-18 21:38:28 +0530

    Added fix in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,14 @@ const Footer = () => {
        <div className="column1 space-y-3">
         <img className='w-20 rounded-lg' src={schoolLogo} alt="School logo" />
         <h3 className='font-semibold text-xl font-heading'>Inodai Waldorf School</h3>
-        <p className='font-body'> Nurturing creativity, imagination and confidence
+        <p className='font-body text-sm  leading-relaxed'> Nurturing creativity, imagination and confidence
             through holistic Waldorf education.</p>
        </div>
 
         {/* column 2 */}
        <div className="column2 space-y-2">
          <h4 className='font-semibold text-xl font-heading '>Quick Links</h4>  
-        <ul className='space-y-2'>
+        <ul className='space-y-2 text-sm leading-relaxed'>
           <li><Link to="/" className="hover:text-accent transition">Home</Link></li>
             <li><Link to="/about" className="hover:text-accent transition">About</Link></li>
             <li><Link to="/courses" className="hover:text-accent transition">Courses</Link></li>
@@ -28,7 +28,7 @@ const Footer = () => {
        {/* column 3 */}
        <div className="column3 space-y-2">
           <h4 className='font-semibold text-xl font-heading '>Programs</h4>
-          <ul className='space-y-2'>
+          <ul className='space-y-2 text-sm leading-relaxed'>
             <li><Link to="/playgroup" className="hover:text-accent transition">Playgroup</Link></li>
             <li><Link to="/kindergarten" className="hover:text-accent transition">Kindergarten</Link></li>
             <li><Link to="/daycare" className="hover:text-accent transition">Day Care</Link></li>
@@ -37,7 +37,7 @@ const Footer = () => {
        {/* column 4 */}
          <div className="column4 space-y-2">
             <h4 className='font-semibold text-xl font-heading '>Contact</h4> 
-            <p className='text-sm leading-relaxed text-gray-200'>
+            <p className='text-sm leading-relaxed font-body text-gray-200'>
               Amba Shanti Chambers, Marol Church Rd,<br />
               Bori Colony, Vijay Nagar Colony West, Marol,<br />
               Andheri East, Mumbai 400 059. <br />
