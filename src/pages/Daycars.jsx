1aa02c654ea3599de643d6c736561c8e87401518
Deep-Rewale <deeprewale2004@gmail.com>
2026-03-08 14:43:18 +0530

    Deploy fix v1

--- src/pages/Daycars.jsx
+++ src/pages/Daycars.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import DayCareHero from "../components/Day-care/DayCareHero";
 import DayCareContent from "../components/Day-care/DayCareContent";
-import DayCareCTA from "../components/Day-care/DayCareCTA.JSX";
+import DayCareCTA from "../components/Day-care/DayCareCTA";
 import Footer from "../components/Footer";
 const Daycars = () => {
   return (
